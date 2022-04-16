d6748ca022549c8fbc10dc0e24306e64d09683d4
kaustubhdeokar <kaustubhd9@gmail.com>
2022-04-16 12:02:06 +0530

    jinja templating added

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+"""Information regarding the Pets in the System."""
+pets = [
+    {"id": 1, "name": "Nelly", "age": "5 weeks",
+     "bio": "I am a tiny kitten rescued by the good people at Paws Rescue Center. I love squeaky toys and cuddles."},
+    {"id": 2, "name": "Yuki", "age": "8 months", "bio": "I am a handsome gentle-cat. I like to dress up in bow ties."},
+    {"id": 3, "name": "Basker", "age": "1 year", "bio": "I love barking. But, I love my friends more."},
+    {"id": 4, "name": "Mr. Furrkins", "age": "5 years", "bio": "Probably napping."},
+]
+
 
 @app.route("/")
 def hello():
     """ Home page. """
-    return "Paws Rescue Center 🐾";
+    return render_template("home.html", pets=pets)
 
 
 @app.route("/about")
 def about():
     """ About page. """
-    return "We are a non-profit organization working as an animal rescue. We aim to help you connect with the purrfect furbaby for you! The animals you find on our website are rescued and rehabilitated animals. Our mission is to promote the ideology \"adopt, don't hop\"! "
+    return render_template("about.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
